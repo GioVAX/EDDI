8373848d11054fa27c24e3e8f178c6f734eb55fb
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2020-10-10 17:28:33 +0200

    Import Integration UI tests

---
+++ IntegrationTests/IntegrationBase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Automation;
+using EddiCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rollbar;
+using Utilities;
+
+namespace IntegrationTests
+{
+    public class IntegrationBase
+    {
+        /// <summary>
+        /// Static code used during tests Setup and Cleanup
+        /// </summary>
+        private static Process _eddi;
+        protected static void LaunchEDDI() =>
+            // Relative path to the local built executable
+            _eddi = Process.Start(@"..\..\..\bin\Debug\EDDI.exe");
+
+        protected static void ShutdownEDDI() =>
+            _eddi?.CloseMainWindow();
+
+        private AutomationElement _mainForm;
+
+        private AutomationElement MainForm => _mainForm ?? (_mainForm = GetMainForm());
+
+        private AutomationElement GetMainForm()
+        {
+            // get reference to main Form control 
+            // a better approach 
+            AutomationElement aeForm = null;
+            for (var numWaits = 0; aeForm == null && numWaits < 50; ++numWaits)
+            {
+                var title = "EDDI v." + Constants.EDDI_VERSION;
+
+                aeForm = AutomationElement.RootElement.FindFirst(TreeScope.Children,
+                    new PropertyCondition(AutomationElement.NameProperty, title));
+                ++numWaits;
+                Thread.Sleep(100);
+            }
+
+            if (aeForm != null)
+                return aeForm;
+
+            throw new Exception("Failed to find EDDI");
+        }
+
+        protected void SelectTab(string tabName)
+        {
+            var tab = MainForm.FindFirst(TreeScope.Descendants,
+                new AndCondition(
+                    new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.TabItem),
+                    new PropertyCondition(AutomationElement.NameProperty, tabName))
+            );
+
+            var sel = (SelectionItemPattern)tab.GetCurrentPattern(SelectionItemPattern.Pattern);
+            sel.Select();
+        }
+
+        protected AutomationElement GetEditBox(string editName) =>
+            MainForm.FindFirst(TreeScope.Descendants,
+                new AndCondition(
+                    new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit),
+                    new PropertyCondition(AutomationElement.AutomationIdProperty, editName))
+            );
+
+
+        protected void MakeSafe()
+        {
+            // Prevent telemetry data from being reported based on test results
+            RollbarLocator.RollbarInstance.Config.Enabled = false;
+
+            // Don't write to permanent storage (do this before we initialize our EDDI instance)
+            Utilities.Files.unitTesting = true;
+
+            // Set ourselves as in a beta game session to stop automatic sending of data to remote systems
+            PrivateObject privateObject = new PrivateObject(EDDI.Instance);
+            privateObject.SetFieldOrProperty("gameIsBeta", true);
+        }
+    }
+}
---
+++ IntegrationTests/WinUIAutomationTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows.Automation;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class WinUIAutomationFunctions : IntegrationBase
+    {
+        [ClassInitialize]
+        public static void Setup(TestContext _) => LaunchEDDI();
+
+        [ClassCleanup]
+        public static void Cleanup() => ShutdownEDDI();
+
+ 
+        [TestMethod]
+        public void CanReadTheSquadronNameTextBox()
+        {
+            SelectTab("Commander Details");
+            var edit = GetEditBox("eddiSquadronNameText");
+
+            var squadronName = (ValuePattern)edit.GetCurrentPattern(ValuePattern.Pattern);
+            squadronName.Current.Value
+                .Should().Be("Newton's Gambit");
+        }
+
+         [TestMethod]
+        public void CanReadTheEDSMCommanderNameTextBox()
+        {
+            SelectTab("EDSM Responder");
+            var edit = GetEditBox("edsmCommanderNameTextBox");
+
+            var edsmCmndrName = (ValuePattern)edit.GetCurrentPattern(ValuePattern.Pattern);
+            edsmCmndrName.Current.Value
+                .Should().Be("GioVAX");
+        }
+
+        [TestMethod]
+        public void ControlsFromUnselectedTabs_SHOULDNotBeAccessible()
+        {
+            SelectTab("EDSM Responder");
+            var edit = GetEditBox("eddiCommanderPhoneticNameText"); // This is in the "Commander Details" tab
+
+            edit.Should().BeNull();
+        }
+    }
+}
